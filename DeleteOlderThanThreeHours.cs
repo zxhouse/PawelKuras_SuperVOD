c96a4b26105fde2aa2566c26204b4a5f754a8b08
Paweł <Pawe@DESKTOP-LV6ARRN>
2019-01-17 23:43:45 +0100

    - dodano prosty program działający na serwerze który usuwa pliki starsze niż 3 godziny w folderach uploads, subtitles oraz previews

---
+++ DeleteOlderThanThreeHours.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true) {
+
+            var dirName = "C:\\xampp\\htdocs\\uploads";
+            string[] files = Directory.GetFiles(dirName);
+
+            foreach (string file in files)
+            {
+                FileInfo fi = new FileInfo(file);
+                if (fi.CreationTime < DateTime.Now.AddHours(-3))
+                    fi.Delete();
+            }
+
+            dirName = "C:\\xampp\\htdocs\\previews";
+            files = Directory.GetFiles(dirName);
+
+            foreach (string file in files)
+            {
+                FileInfo fi = new FileInfo(file);
+                if (fi.CreationTime < DateTime.Now.AddHours(-3))
+                    fi.Delete();
+            }
+
+            dirName = "C:\\xampp\\htdocs\\subtitles";
+            files = Directory.GetFiles(dirName);
+
+            foreach (string file in files)
+            {
+                FileInfo fi = new FileInfo(file);
+                if (fi.CreationTime < DateTime.Now.AddHours(-3))
+                    fi.Delete();
+            }
+
+            }
+
+        }
+    }
+}
